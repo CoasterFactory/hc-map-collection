004b628af394cd1ce188d52c9cd18217ebf3b805
Vincent Wilkie <vwilkie@coasterfactory.com>
2018-09-12 12:56:36 -0400

    Adding custom/world to map collection

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // index.js
 
 import usaAndCanada from './usa-and-canada'
+import world from './world'
 
 var hcMaps = {
-  'custom/usa-and-canada': usaAndCanada
+  'custom/usa-and-canada': usaAndCanada,
+  'custom/world': world
 }
 
-console.log('hcMaps', hcMaps)
-
 export default function (Highcharts, maps) {
-  console.log('hc-map-collection', Highcharts, maps)
+  maps.forEach(function (map) {
+    console.log(map)
+
+    // Highcharts.maps[map] = hcMaps[map] || world
+  })
 }
